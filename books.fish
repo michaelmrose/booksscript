47cd6da72db62aef796ba615fec89fcea96eaa9b
Michael Rose <michael@rosenetwork.net>
2014-08-25 19:04:19 -0700

    current state

--- books.fish
+++ books.fish
@@ -1,6 +1,8 @@
 function books
     set selector $argv[1]
     set criteria author title publisher series tag tags
+    set formats pdf epub lit txt
+    
     if contains $selector $criteria
         set query $argv[2..-1]
     else
@@ -8,51 +10,70 @@ function books
         set query $argv
     end
 
-    # set filenames (find-books $query)
     set filenames (find-books2 $selector $query)
-    set pdfs (filter filenames .pdf)
-    set epubs (filter filenames .epub)
-    set filenames $epubs $pdfs
+    # set filenames (find-books $query)
     set books (apply extract-title filenames | sort | uniq)
-    
-    set nbooks (count $books)
 
+    for i in $filenames
+        set filetype (extract-filetype $i)
+        set $filetype $filetpe $i
+    end
+
+    # echo pdfs
+    # println $pdf
+    # echo epub
+    # println $epub
+    # echo filetypes
+    # echo $filetypes | sort | uniq
+        
     # echo query $query
     # echo filenames $filenames
-    # echo pdfs $pdfs
-    # echo epubs $epubs
+    # echo pdfs $pdf
+    # echo epubs $epub
     # echo books $books
+
+    set nbooks (count $books)
     # echo nbooks $nbooks
 
     switch $nbooks
         case 0
             echo "Search produced no results"
         case 1
-            if contains $pdfs $filenames
-                open $pdfs &
-            else 
-                open $epubs &
+            for i in $formats
+                if contains $$i $filenames
+                    open $$i
+                    return 0
+                end
             end
             
         case "*"
             # given many results find out if one is an exact match
             for i in $books
-                if match $i $query
+                set val (echo $i | trim)
+                if match $val $query
                     #ok so one is an exact match
                     for j in $filenames
                         set title (echo $j | extract-title)
                         # lets find the filename which contains the title 
                         # that we want and open it
                         if match $title $query
-                            open $j
+                            # add everything that matches to $matches
+                            set matches $matches $j
                         end
                     end
-                    return 0
+                    #now find a each of the suitable formats see if there is a match for it in filenames
+                    for i in $formats
+                        if contains $$i $filenames
+                            open $$i
+                            return 0
+                        end
+                    end
                 end
             end
             # so no exact match so show a menu and pipe the users choice to 
             # books where it will now be an exact match 
             fuzzymenu (println $books | sed 's/_/:/g')
+            println $books
             #fuzzymenu sets global fquery variable because fzf is broken within variable sub in fish
             books $fquery
         end
@@ -73,16 +94,29 @@ function find-books2
     println $returnval
  end
 
-function find-book-id
-    calibredb list -s "title:$argv" -f formats --for-machinee| grep '"id":' | cut -d ":" -f2 | cut -d '"' -f2 | cut -d "," -f1 | trim
+function find-exact-book
+    set query $argv
+    set returnval (find-books2 title $query | extract-title | grep -i -E "^$query\$" | sort | uniq)
+    println $returnval
 end
 
 function extract-title
     if exists $argv
-        echo $argv | cut -d "." -f1 | cut -d "/" -f7 | cut -d "(" -f1 | trim
+        echo $argv | cut -d "." -f1 | cut -d "/" -f7 | cut -d "(" -f1 | trim | sed 's/\\\//g'
     else
         while read -l line
-            echo $line | cut -d "." -f1 | cut -d "/" -f7 | cut -d "(" -f1 | trim
+            echo $line | cut -d "." -f1 | cut -d "/" -f7 | cut -d "(" -f1 | trim | sed 's/\\\/ /g'
         end
+    end
+end
+
+function extract-filetype
+    if exists $argv
+        echo (cutlast . $argv)
+    else
+        while read -l line
+            echo (cutlast . $line)
+        end
+        return 0
     end
 end
